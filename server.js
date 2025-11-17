e6b8c4a878e599f4c2f07eeadd462f925680e492
André Luiz Fortunato <159827240+AndreFortunato6@users.noreply.github.com>
2025-11-17 16:35:54 -0300

    Update server.js
    update 3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // --- INICIALIZA GEMINI ---
